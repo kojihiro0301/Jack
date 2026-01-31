b66272da41ba641bce67ed06ecb571d306646934
ソプ <ckdtjqrns1196@gmail.com>
2026-01-31 22:30:05 +0900

    シーン移動機能の改善

--- TeamJack/Assets/Sopoo/_C#/SceneChangeManager.cs
+++ TeamJack/Assets/Sopoo/_C#/SceneChangeManager.cs
@@ -3,13 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeManager : MonoBehaviour
 {
-    // Game Start 버튼에 연결할 함수
     public void LoadPlayScene()
     {
         SceneManager.LoadScene("PlayScene"); 
     }
-
-    // Play Again 버튼에 연결할 함수
     public void LoadTitleScene()
     {
         SceneManager.LoadScene("TitleScene");
